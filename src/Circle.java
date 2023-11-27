1f1963b9f3f3441e65dd9c97ef2264a9cea49bde
PRogr4m4d0r <victor.ascencio@davinci.edu.ar>
2023-11-27 13:32:59 +0100

    feat: agregue la clase Circle

---
+++ src/Circle.java
@@ -0,0 +1,20 @@
+class Circle extends Shape {
+    private double radius = 1.0;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    public String toString(){
+        return "";
+    }
+}
