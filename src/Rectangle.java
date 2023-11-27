c4b5cb18359a0451dea3c3c7158040e9f13f11b4
PRogr4m4d0r <victor.ascencio@davinci.edu.ar>
2023-11-27 13:40:18 +0100

    feat: agregue la clase reactangle

---
+++ src/Rectangle.java
@@ -0,0 +1,46 @@
+class Rectangle extends Shape {
+
+    private double width = 1.0;
+    private double height = 1.0;
+
+    public Rectangle() {
+        this.width = 1.0;
+        this.height = 1.0;
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{width=" + width + ", height=" + height + '}';
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
